0e7b2a6ecf5efb367ee2540e1634d9d915385505
Belle P <bellepx3@gmail.com>
2019-09-17 01:39:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,6 +20,18 @@ get '/say/:word1/:word2/:word3/:word4/:word5' do
 end
 
 get '/:operation/:number1/:number2' do
-params[:operation]
+if params[:operation] == add
+  @sum = params[:number1].to_i + params[:number2].to_i
+  @sum.to_s
+  elsif params[:operation] == subtract
+  @difference = params[:number1].to_i - params[:number2].to_i
+  @difference.to_s
+  elsif params[:operation] == product
+   @product = params[:number1].to_i * params[:number2].to_i
+  @product.to_s
+else 
+  params[:operation] == quotient
+   @quotient= params[:number1].to_i / params[:number2].to_i
+  @quotient.to_s
 
 end
